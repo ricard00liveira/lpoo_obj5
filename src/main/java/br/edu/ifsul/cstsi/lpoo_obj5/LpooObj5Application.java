9563fc8b8894c21db01aebd4d1c0c1dc0eed3bae
Ricardo P Oliveira <ricardooliveira.pl216@academico.ifsul.edu.br>
2023-11-15 09:51:33 -0300

    Novo repositorio

---
+++ src/main/java/br/edu/ifsul/cstsi/lpoo_obj5/LpooObj5Application.java
@@ -0,0 +1,14 @@
+package br.edu.ifsul.cstsi.lpoo_obj5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LpooObj5Application {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(LpooObj5Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/ifsul/cstsi/lpoo_obj5/LpooObj5ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifsul.cstsi.lpoo_obj5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LpooObj5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
